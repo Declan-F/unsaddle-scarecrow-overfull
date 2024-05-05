380ab1d02b8923a04045e09672941b3b21f51fff
Declan Fodor <me@declanfodor.com>
2024-05-05 02:02:08 -0500

    hello world in assembly

--- myfile.c
+++ myfile.c
@@ -1,6 +1,10 @@
+#include <sys/syscall.h>
 int foo() 
 {
-    return 1 + 1;
+    
+    int arr[10] = { 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10 };
+    return arr[5] + arr[6];
+
 }
 
 
--- test.asm
+++ test.asm
@@ -2,10 +2,29 @@
 .intel_syntax noprefix
 .globl _start
 
-foo:
-    mov rax, 0x3c
+main:
+    # write syscall
+    mov rax, 0x1
+    # write syscall arguments
+    mov rdi, 0x1
+    mov rsi, rsp
+    sub rsi, 0x10
+    mov rdx, 0x0c
+    # write string to stack
+    mov QWORD PTR [rsi], 0x6c6c6548
+    mov QWORD PTR [rsi+4], 0x6f77206f
+    mov QWORD PTR [rsi+8], 0x21646c72
+    syscall
+    # Not sure if zeroing everything is required, but probably good to prevent side effects
+    mov QWORD PTR [rsi], 0x00000000
+    mov QWORD PTR [rsi+4], 0x00000000
+    mov QWORD PTR [rsi+8], 0x00000000
+    mov rdi, 0x0
+    mov rsi, 0x0
+    mov rdx, 0x0
     ret
 
 _start:
-    call foo
+    call main
+    mov rax, 0x3c
     syscall
